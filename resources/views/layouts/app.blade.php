2efaf176ff260a5a47e50f38b84980b306e9c848
CpasWalass <walasslawadan@gmail.com>
2026-06-26 07:37:31 +0000

    feat: archived reports drawer via btn-apps in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,8 +78,13 @@
                 </button>
                 @endauth
 
-                {{-- Grille apps --}}
-                <button class="p-2 rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800 transition hidden sm:block" id="btn-apps">
+                {{-- Archives rapports clôturés --}}
+                <button
+                    onclick="window.dispatchEvent(new CustomEvent('open-archives'))"
+                    class="p-2 rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800 transition hidden sm:block"
+                    id="btn-apps"
+                    title="Rapports archivés"
+                >
                     <svg class="w-5 h-5 text-gray-600 dark:text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"/>
                     </svg>
